31793f8b952ae3ac008ebab90988f59c6de8fcad
vitorvale007 <vitorvale007@gmail.com>
2019-03-24 18:12:12 +0000

    Codigo com estrutura base

--- src/proj1.c
+++ src/proj1.c
@@ -5,37 +5,68 @@
 #define MAX_SALAS 10
 #define MAX_EVENTOS_SALA 100
 #define MAX_PARTICIPANTES 3
+#define MAX_NAMES_LENGTH 64
+#define MAX_COMPONENTES 10
 
 /*    char data[9]; */
 /*    char hora[5]; */
 
 typedef struct {
-    char descricao[64];
+    char descricao[MAX_NAMES_LENGTH];
     struct tm data;
     int duracao;
     int sala;
-    char responsavel[64];
-    char participantes[(64 * 3) + 3];
+    char responsavel[MAX_NAMES_LENGTH];
+    char participantes[(MAX_NAMES_LENGTH * 3) + 3];
 } Evento;
 
+/* TODO: ver se isto faz sentido */
 typedef struct {
     Evento eventos[100];
 } Sala;
 
 void listaEventos(Evento eventos[], int numeroEventos);
 int criaEvento(Evento eventos[], int numeroEventos, char camposEvento[]);
+void parteString(char componentes[][MAX_NAMES_LENGTH], char params[]);
+char lerComando(char params[]);
 
-int main(int argc, char *argv[]) {
-    int c;
+int main() {
+    char c;
+	int idx;
 
+	/* TODO: adicionar comentarios a explicar o que isto e */
     Evento eventos[MAX_SALAS * MAX_EVENTOS_SALA];
     int numeroEventos = 0;
+	char params [342]; /* 63 + 8 + 4 + 4 + 2 + 63 * 4 + 9 (':') */
+	/* Ex: a Reuniao dos Ze:29032019:1000:60:1:Ze:Ze Carlos:Ze Pedro:Ze Joao */
 
-    /*
-    for (c = 0; c < argc; c++) {
-        printf("%d argument is %s\n", c + 1, argv[c]);
-    }
-    */
+	char componentes[MAX_COMPONENTES][MAX_NAMES_LENGTH];
+	char cmd;
+
+	/* Ler comando */
+	scanf("%c", &cmd);
+
+	/* Ler restantes parametros, caso existam, para uma unica string */
+	idx = 0;
+	while((c = getchar()) != '\n') {
+		if (idx == 0 && c == ' ') {
+			continue;
+		}
+		params[idx++] = c;
+	}
+	params[idx] = '\0';
+
+	printf("Command: [%c]\n", cmd);
+	printf("Params: [%s]\n", params);
+
+	/* Partir a string, caso exista, nos diversos componentes */
+	parteString(componentes, params);
+
+/*
+	switch(cmd) {
+		case 'a': numeroEventos = criaEvento(eventos, numeroEventos, argv[][]);
+	}
+*/
 
     numeroEventos = criaEvento(eventos, numeroEventos, "");
     numeroEventos = criaEvento(eventos, numeroEventos, "");
@@ -73,5 +104,32 @@ int criaEvento(Evento eventos[], int numeroEventos, char camposEvento[]) {
     eventos[numeroEventos++] = evento;
 
     return numeroEventos;
+}
+
+void parteString(char componentes[][MAX_NAMES_LENGTH], char params[]) {
+    int i = 0, idx = 0, compIdx = 1;
+	char c;
+	char componente[MAX_NAMES_LENGTH];
+
+	/* Reuniao dos Ze:29032019:1000:60:1:Ze:Ze Carlos:Ze Pedro:Ze Joao */
+	while ((c = params[i++]) != '\0') {
+		if(c == ':') {
+			componente[idx] = '\0';
+			strcpy(componentes[compIdx++], componente);
+			memset(componente, '\0', sizeof(componente));
+			idx = 0;
+		} else {
+			componente[idx++] = c;
+		}
+	}
+	/* adicionar ultimo elemento */
+	strcpy(componentes[compIdx++], componente);
+
+	/* Adicionar o numero de componentes na primeira posicao */
+	sprintf(componentes[0], "%d", (compIdx - 1));
+
+	for ( i = 0; i < compIdx; i++) {
+		printf("Componente [%d]: [%s]\n", i, componentes[i]);
+	}
 }
 
